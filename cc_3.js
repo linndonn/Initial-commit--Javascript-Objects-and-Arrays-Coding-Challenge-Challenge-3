9f387f5f061f845aa97d1c2eacdf36b280761d6c
linndonn <linndonn@usf.edu>
2025-02-02 17:08:58 -0500

    Task 1 - Product Price Management.

---
+++ cc_3.js
@@ -0,0 +1,11 @@
+// Task 1 - Product Price Management Scenario
+
+let price = [20, 40, 60, 80, 100];
+//Initialize array prices using aleast 5 numerical values
+price.push(120); 
+//Adding a new price to the array
+price.shift();
+//Remove  the first price from the array
+console.log ("Update Prices:", price);
+//Log the updated price list 
+
