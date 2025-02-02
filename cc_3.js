6a94b79728e44b810536baeb3e873ad25e38525f
linndonn <linndonn@usf.edu>
2025-02-02 17:29:24 -0500

    Task 2 - Modifying Customer Orders.

--- cc_3.js
+++ cc_3.js
@@ -9,3 +9,15 @@ price.shift();
 console.log ("Update Prices:", price);
 //Log the updated price list 
 
+// Task 2 - Modifying Customer Orders Scenario
+
+let orders = [50, 60, 70, 80, 90];
+// Declare an array orders with at least five numerical values representing quantities. 
+orders[2] += 5;
+// Increase the third order quantity by 5
+const TotalOrders = orders.reduce((TotalNumbers, orders) => TotalNumbers + orders, 0);
+// Calculate the total number of all orders
+console.log("Updated Orders:", orders);
+// Print the update list of orders
+console.log("Total Order Count:", TotalOrders);
+// Print the overall total order.
